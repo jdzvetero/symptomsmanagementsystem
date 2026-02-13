c676459a9714902c0876ca538a34fff7cc3214f7
TinkerJenny <jennifer.dzvetero@students.uz.ac.zw>
2026-02-13 10:00:01 +0200

    Initial commit - Symptoms Management System

---
+++ src/main/java/com/healthcare/symptomssys/SymptomssysApplication.java
@@ -0,0 +1,13 @@
+package com.healthcare.symptomssys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymptomssysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SymptomssysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/healthcare/symptomssys/SymptomssysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.symptomssys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymptomssysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
